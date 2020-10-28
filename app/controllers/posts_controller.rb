28416d9af24b2685e3b74824ddd966f0a90859a1
yunayotuba <73088676+yunayotuba@users.noreply.github.com>
2020-10-28 15:45:19 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,13 @@
 class PostsController < ApplicationController
+
   def index
+    @posts = Post.all.order(id: "DESC")
   end
-end
+ 
+ 
+  def create
+    Post.create(content: params[:content])
+    redirect_to action: :index
+  end
+ 
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
- end
+  root to: 'posts#index'
+# get 'posts/new', to: 'posts#new' ←削除
+  post 'posts', to: 'posts#create'
+end
